8523e7b7796151b51777abd04142d0b73527641a
rodrigo.gomes <rodrigo.gomes@dotzmkt.com.br>
2017-09-28 12:54:45 -0300

    Nova versao de meteorjs

--- apechincha.com.br/v1/client/main.js
+++ apechincha.com.br/v1/client/main.js
@@ -1,4 +1,7 @@
-import './hello.html';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
