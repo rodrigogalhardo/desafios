ccbccb9429708cc517e1fa3c387c42a930a8f17f
galhardo <29082130>
2017-10-15 12:38:45 -0200

    ajuste de rotas

--- apechincha.com.br/v1/src/routes/index.js
+++ apechincha.com.br/v1/src/routes/index.js
@@ -1,5 +1,4 @@
 module.exports = (app) => {
     app.use('/', require('./landing'))
     app.use('/produtos', require('./produtos'))
-    app.use('/detalhe-produto', require('./produtos/detalhe_produto'))
 }
--- apechincha.com.br/v1/src/routes/produtos/index.js
+++ apechincha.com.br/v1/src/routes/produtos/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import produtos from './../../services/produtos/'
+import detalhes_produtos from './../../services/produtos/detalhe_produto'
 
 const router = express.Router()
 router.get('/', produtos)
+router.get('/produtos/detalhe-produto/:id', detalhes_produtos)
 
 module.exports = router
